fe301180dbca916ef06b75ae5e82ba63d54db309
Roberto Jiménez <powermx@gmail.com>
2023-12-28 23:47:20 -0600

    ZiVPN V2

--- ziv2.sh
+++ ziv2.sh
@@ -1,20 +1,21 @@
-# Zivpn UDP Module installer v2 
+#!/bin/bash
+# Zivpn UDP Module installer
 # Creator Zahid Islam
 
 echo -e "Updating server"
 sudo apt-get update && apt-get upgrade -y
-systemctl stop zivpn.service 1> /dev/null 2> /dev/null
+systemctl stop zivpn_backfill.service 1> /dev/null 2> /dev/null
 echo -e "Downloading UDP Service"
-wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64_backfill -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
-chmod +x /usr/local/bin/zivpn
+wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64_backfill -O /usr/local/bin/zivpn_backfill 1> /dev/null 2> /dev/null
+chmod +x /usr/local/bin/zivpn_backfill
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
-wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config_backfill.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
+wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config_backfill.json -O /etc/zivpn/config_backfill.json 1> /dev/null 2> /dev/null
 
 echo "Generating cert files:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
 sysctl -w net.core.rmem_max=16777216 1> /dev/null 2> /dev/null
 sysctl -w net.core.wmem_max=16777216 1> /dev/null 2> /dev/null
-cat <<EOF > /etc/systemd/system/zivpn.service
+cat <<EOF > /etc/systemd/system/zivpn_backfill.service
 [Unit]
 Description=zivpn VPN Server
 After=network.target
@@ -23,7 +24,7 @@ After=network.target
 Type=simple
 User=root
 WorkingDirectory=/etc/zivpn
-ExecStart=/usr/local/bin/zivpn -c /etc/zivpn/config.json server
+ExecStart=/usr/local/bin/zivpn_backfill server -c /etc/zivpn/config_backfill.json
 Restart=always
 RestartSec=3
 Environment=ZIVPN_LOG_LEVEL=info
@@ -35,26 +36,18 @@ NoNewPrivileges=true
 WantedBy=multi-user.target
 EOF
 
-echo -e "ZIVPN UDP Usernames/Passwords"
-read -p "Enter usernames separated by commas, example: user1,user2 (Press enter for Default 'zi'): " input_config
+echo -e "ZIVPN UDP new Username/Password"
+read -p "Enter username/password, example: user1 (Press enter for Default 'zi'): " input_config
 
-if [ -n "$input_config" ]; then
-    IFS=',' read -r -a config <<< "$input_config"
-    if [ ${#config[@]} -eq 1 ]; then
-        config+=(${config[0]})
-    fi
-else
-    config=("zi")
-fi
+config=${input_config:-"zi"}
+configstr="\"config\": \"$config\""
+sed -i -E 's/"config":\s*"[^"]*"/'"$configstr"'/g' /etc/zivpn/config_backfill.json
 
-new_config_str="\"config\": [$(printf "\"%s\"," "${config[@]}" | sed 's/,$//')]"
 
-sed -i -E "s/\"config\": ?\[[[:space:]]*\"zi\"[[:space:]]*\]/${new_config_str}/g" /etc/zivpn/config.json
-
-systemctl enable zivpn.service
-systemctl start zivpn.service
+systemctl enable zivpn_backfill.service
+systemctl start zivpn_backfill.service
 iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 6000:19999 -j DNAT --to-destination :5667
 ufw allow 6000:19999/udp
 ufw allow 5667/udp
-rm ziv2.* 1> /dev/null 2> /dev/null
-echo -e "Installed"
+rm zi2.* 1> /dev/null 2> /dev/null
+echo -e "Backfill Installed"
