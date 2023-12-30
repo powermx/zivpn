af34387f3e1c8a1c912fbd1a91e8e92be0dd1c96
Roberto Jiménez <powermx@gmail.com>
2023-12-30 12:16:14 -0600

    ZiVPN V2

--- ziv2.sh
+++ ziv2.sh
@@ -36,16 +36,21 @@ NoNewPrivileges=true
 WantedBy=multi-user.target
 EOF
 
-echo -e "ZIVPN UDP new Password"
-read -p "Enter passwords separated by commas, example: pass1,pass2 (Press enter for Default 'zi'): " input_password
-IFS=',' read -ra password_array <<< "$input_password"
-if [ -z "$input_password" ]; then
-    password_array=("zi")
+echo -e "ZIVPN UDP Usernames/Passwords"
+read -p "Enter usernames separated by commas, example: user1,user2 (Press enter for Default 'zi'): " input_config
+
+if [ -n "$input_config" ]; then
+    IFS=',' read -r -a config <<< "$input_config"
+    if [ ${#config[@]} -eq 1 ]; then
+        config+=(${config[0]})
+    fi
+else
+    config=("zi")
 fi
-configstr="\"config\": [\"$(IFS=\",\"; echo "${password_array[*]}")\"]"
-sed -i -E 's/"config":\s*\[[^\]]*\]/'"$configstr"'/g' /etc/zivpn/config_backfill.json
 
+new_config_str="\"config\": [$(printf "\"%s\"," "${config[@]}" | sed 's/,$//')]"
 
+sed -i -E "s/\"config\": ?\[[[:space:]]*\"zi\"[[:space:]]*\]/${new_config_str}/g" /etc/zivpn/config_backfill.json
 
 systemctl enable zivpn_backfill.service
 systemctl start zivpn_backfill.service
